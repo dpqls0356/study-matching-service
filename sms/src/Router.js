52cd48a3f87b592ab1a557a34dea50a79cb9d7c5
dpqls0356 <dpqls0356@gmail.com>
2023-08-29 02:50:31 +0900

    front - 회원 가입 페이지 생성 + form 데이터 받기
    useForm 사용해서 form 데이터 받아옴

--- sms/src/Router.js
+++ sms/src/Router.js
@@ -1,15 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./screens/Home";
+import Join from "./screens/users/Join";
 
-const router = createBrowserRouter({
-    path:"/",
-    element:<App/>,
-    childrend:[
-        {
-            path:"",
-            element:<Home/>,
-        }
-    ]
-});
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+            children:[
+                {
+                    path:"",
+                    element:<Home/>,
+              },
+                {
+                    path:"/join",
+                    element:<Join/>,
+                }
+        ],
+    }
+]);
 export default router;
